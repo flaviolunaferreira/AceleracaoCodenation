5eea317679dc691858cdda29f96c1753a7460d88
flavio luna ferreira <flaviolunaferreira@gmail.com>
2021-07-06 16:28:43 -0300

    incluindo consulta generica

--- src/main/java/com/cit/projetoPratico/Controller/ErrosController.java
+++ src/main/java/com/cit/projetoPratico/Controller/ErrosController.java
@@ -1,15 +1,13 @@
 package com.cit.projetoPratico.Controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +16,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cit.projetoPratico.Model.Entity.ErrosEntity;
 import com.cit.projetoPratico.Model.Repository.ErrosJpaRepository;
@@ -33,8 +33,9 @@ public class ErrosController {
 	
 	@Autowired
 	private ErrosJpaRepository jpaRepository;
-
+	
 	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED )
 	public ErrosEntity saveErros(@RequestBody ErrosEntity errosEntity) {
 		repository.save(errosEntity);
 		return errosEntity;
@@ -57,10 +58,9 @@ public class ErrosController {
 	
 	@GetMapping("/id/{id}")
 	@ResponseBody
-	public ResponseEntity<?> getErrosById(@PathVariable Long id) { 			
-		Optional<ErrosEntity> errosEntity = repository.findById(id);
-		if (errosEntity.isPresent()) return ResponseEntity.ok(errosEntity.get());
-		return ResponseEntity.notFound().build();
+	public ErrosEntity getErrosById(@PathVariable Long id) { 			
+		return repository.findById(id).orElseThrow( () -> 
+		new ResponseStatusException(HttpStatus.NOT_FOUND, "Erro não encontrado"));
 	}
 	
 	@PutMapping
@@ -68,9 +68,11 @@ public class ErrosController {
 	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody ErrosEntity errosEntity) {
 		return repository
 				.findById(id)
+				
 				.map( erroId -> {
 					errosEntity.setId(erroId.getId());
 					repository.save(errosEntity);
+					
 					return ResponseEntity.ok(errosEntity);
 				}).orElseGet( () -> ResponseEntity.notFound().build() ); 	
 	}
@@ -88,7 +90,7 @@ public class ErrosController {
                                     .withStringMatcher(StringMatcher.CONTAINING);
 
         Example<ErrosEntity> example = Example.of(filtro, matcher);
-        List<ErrosEntity> lista = JpaRepository.findAll(example, page);
+        Page<ErrosEntity> lista = jpaRepository.findAll(example, page);
         return ResponseEntity.ok(lista);
     }
 
