393764c39720c21e9bafa1a5f32a34bc09ebf624
flavio luna ferreira <flaviolunaferreira@gmail.com>
2021-07-06 16:36:07 -0300

    liberando a porta 300

--- src/main/java/com/cit/projetoPratico/Controller/ErrosController.java
+++ src/main/java/com/cit/projetoPratico/Controller/ErrosController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import com.cit.projetoPratico.Model.Entity.ErrosEntity;
 import com.cit.projetoPratico.Model.Repository.ErrosJpaRepository;
 import com.cit.projetoPratico.Model.Repository.ErrosRepository;
 
+@CrossOrigin(origins = "http://localhost:3000") //so requesições deste endereço que vão ser respondidas
 @RestController
 @RequestMapping("/api")
 public class ErrosController {
