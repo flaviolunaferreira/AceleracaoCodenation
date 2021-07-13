6a2c314b09ff3861efe69945f734e0ee69e4f77a
danieleperse <danieleperse3@gmail.com>
2021-07-13 20:21:58 -0300

    swagger incluso na exceçao do oauth

--- src/main/java/com/cit/projetoPratico/security/SecurityConfiguration.java
+++ src/main/java/com/cit/projetoPratico/security/SecurityConfiguration.java
@@ -27,10 +27,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		return super.authenticationManagerBean();
 	}
 	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/swagger-ui.html");
-	}
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources/**",
+                                   "/configuration/security",
+                                   "/swagger-ui.html",
+                                   "/webjars/**");
+    }
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
