d62792010c19e8473848ee3e8ed0010079641d41
jorgehsosiro <jojimurakabe@gmail.com>
2021-07-17 17:14:10 -0300

    update

--- src/main/java/com/cit/projetoPratico/Controller/ErrosController.java
+++ src/main/java/com/cit/projetoPratico/Controller/ErrosController.java
@@ -1,7 +1,5 @@
 package com.cit.projetoPratico.Controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -12,9 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
